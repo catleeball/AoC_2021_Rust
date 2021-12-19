93c1789807b0e62d75f1051709c38ddad2c7c5d2
Cat Lee Ball 🎶🎷🐀 <cat@catball.dev>
2021-12-19 12:10:44 -0800

    Make from_str a method of Instruction

--- src/day_two.rs
+++ src/day_two.rs
@@ -6,25 +6,27 @@ pub fn run_day_two() {
 }
 
 enum   Direction   { Up, Down, Forward }
-struct Instruction { dir: Direction, qty: u16 }
 struct Position    { x: u16, depth: u16 }
+struct Instruction { dir: Direction, qty: u16 }
 
-fn instruction_from_str(s: &str) -> Instruction {
-    let (dir_str, qty_str) = s.split_once(' ').unwrap_or(("forward", "0"));
-    let qty: u16 = qty_str.parse().unwrap_or(0);
-    let dir: Direction = match dir_str {
-        "up"      => Direction::Up,
-        "down"    => Direction::Down,
-        "forward" => Direction::Forward,
-        _         => panic!("Malformed direction: {}", dir_str),
-    };
-    Instruction {dir, qty}
+impl Instruction {
+    fn from_str(s: &str) -> Instruction {
+        let (dir_str, qty_str) = s.split_once(' ').unwrap_or(("forward", "0"));
+        let qty: u16 = qty_str.parse().unwrap_or(0);
+        let dir: Direction = match dir_str {
+            "up" => Direction::Up,
+            "down" => Direction::Down,
+            "forward" => Direction::Forward,
+            _ => panic!("Malformed direction: {}", dir_str),
+        };
+        Instruction { dir, qty }
+    }
 }
 
 fn part_one() -> u32 {
     let final_position = file_to_str("./src/day_two.input.txt")
         .lines()
-        .map(instruction_from_str)
+        .map(Instruction::from_str)
         .fold(Position{x: 0, depth: 0}, |mut pos, instruct| {
             match instruct.dir {
                 Direction::Up      => { pos.depth -= instruct.qty; pos }
