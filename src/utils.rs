a422d205e5c1b29014bbac5a41a4c67ebe7126b5
Cat Lee Ball 🎶🎷🐀 <cat@catball.dev>
2021-12-18 20:26:12 -0800

    Lint fix for style; remove `return`.

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,7 @@ use std::fs;
 // Turn a file into a string or die.
 pub fn file_to_str(filepath: &str) -> String {
     match fs::read_to_string(filepath) {
-        Ok(lines) => { return lines; },
+        Ok(lines) => { lines },
         Err(_) => { panic!("D1P1 Error: File not found: {}", filepath) }
     }
 }
